00b4ac126802447a55573f3baf617a4fe8d5a62e
Abhemanyu Sarin <abhemanyus@gmail.com>
2022-12-14 11:27:46 +0530

    day ten, part one

---
+++ src/bin/ten.rs
@@ -0,0 +1,76 @@
+use advent_of_code::load_file;
+
+fn main() {
+    let data = load_file("ten");
+    let instructions = data.split("\n").map(Instruction::from);
+    let mut cpu = CPU::new(instructions);
+    let mut accumulator = 0;
+    while cpu.tick().is_some() {
+        // println!(
+        //     "Register: {}, Cycle: {}, instruction: {:?}",
+        //     cpu.register, cpu.cycle, cpu.instruction
+        // );
+        if (cpu.cycle - 20) % 40 == 0 {
+            accumulator += cpu.register * cpu.cycle;
+            println!("Register: {}, Accumulator: {}", cpu.register, accumulator);
+        }
+    }
+    println!("Total cycles: {}, accumulator: {}", cpu.cycle, accumulator);
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Instruction {
+    Noop,
+    Addx(i32),
+}
+
+impl From<&str> for Instruction {
+    fn from(value: &str) -> Self {
+        match value {
+            "noop" => Self::Noop,
+            addx => Self::Addx(addx.split_at(5).1.parse().unwrap()),
+        }
+    }
+}
+
+impl Instruction {
+    fn steps(&self) -> i32 {
+        match self {
+            Instruction::Noop => 1,
+            Instruction::Addx(_) => 2,
+        }
+    }
+}
+
+struct CPU<T: Iterator<Item = Instruction>> {
+    cycle: i32,
+    instructions: T,
+    instruction: Instruction,
+    register: i32,
+    steps: i32,
+}
+
+impl<T: Iterator<Item = Instruction>> CPU<T> {
+    fn new(instructions: T) -> Self {
+        CPU {
+            cycle: 0,
+            instructions,
+            instruction: Instruction::Noop,
+            register: 1,
+            steps: 1,
+        }
+    }
+    fn tick(&mut self) -> Option<()> {
+        if self.steps == self.instruction.steps() {
+            match self.instruction {
+                Instruction::Noop => (),
+                Instruction::Addx(v) => self.register += v,
+            }
+            self.instruction = self.instructions.next()?;
+            self.steps = 0;
+        }
+        self.steps += 1;
+        self.cycle += 1;
+        Some(())
+    }
+}
